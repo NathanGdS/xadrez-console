a8f67fbf5f880ed4e8a1478bc3e9a213b2af0166
Nathan Garcia <nathangar01@gmail.com>
2020-12-22 20:03:10 -0300

    Comecando a implementar o Tabuleiro

---
+++ xadrez-console/tabuleiro/Cor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    enum Cor
+    {
+        White,
+        Black,
+        Yellow,
+        Blue,
+        Green,
+        Orange
+    }
+}
---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QntMovimentos { get; protected set; }
+        public Tabuleiro Tab { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            this.Posicao = posicao;
+            this.Cor = cor;
+            this.Tab = tab;
+            this.QntMovimentos = 0;
+        }
+    }
+}
