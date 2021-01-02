a07fb19431be124e33ccc3b7fe7ebd0887a55cdf
Nathan Garcia <nathangar01@gmail.com>
2021-01-02 19:55:57 -0300

    Validando a posicao de Origem

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -15,12 +15,24 @@
             this.QntMovimentos = 0;
         }
 
-        public abstract bool[,] MovimentosPossiveis();
-
         public void IncrementarQntDeMovimentos()
         {
             this.QntMovimentos++;
         }
+
+        public bool ExisteMovimentosPossiveis()
+        {
+            bool[,] mat = MovimentosPossiveis();
+            for(int i=0; i<Tab.Linhas; i++) {
+                for(int j=0; j<Tab.Colunas; j++) {
+                    if(mat[i, j]) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public abstract bool[,] MovimentosPossiveis();
 
     }
 }
--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -5,13 +5,13 @@ namespace xadrez
     class PartidaDeXadrez
     {
         public Tabuleiro Tab { get; private set; }
-        private int Turno { get; set; }
-        private Cor JogadorAtual { get; set; }
+        public int Turno { get; private set; }
+        public Cor JogadorAtual { get; private set; }
         public bool Terminada { get; private set; }
 
         public PartidaDeXadrez()
         {
-            this.Tab = new Tabuleiro(8,8);
+            this.Tab = new Tabuleiro(8, 8);
             this.Turno = 1;
             this.JogadorAtual = Cor.White;
             this.Terminada = false;
@@ -25,13 +25,49 @@ namespace xadrez
             Tab.RetirarPeca(destino);
             Peca PecaCapturada = Tab.RetirarPeca(destino);
             Tab.ColocarPeca(p, destino);
-            
+
+        }
+
+        public void RealizaJogada(Posicao origem, Posicao destino)
+        {
+            ExecutaMovimento(origem, destino);
+            this.Turno++;
+            MudaJogador();
+        }
+
+        public void ValidarPosicaoDeOrigem(Posicao pos)
+        {
+            if (Tab.peca(pos) == null) {
+                throw new TabuleiroException("Nao existe uma peca nesta posicao!");
+            }
+
+            if (JogadorAtual != Tab.peca(pos).Cor) {
+                throw new TabuleiroException("A peca de origem escolhida nao e sua!");
+            }
+
+            if (!Tab.peca(pos).ExisteMovimentosPossiveis()) {
+                throw new TabuleiroException("Nao existem movimentos possiveis para esta peca de origem!");
+            }
+
+        }
+
+        private void MudaJogador()
+        {
+            if (this.JogadorAtual == Cor.White) {
+                this.JogadorAtual = Cor.Black;
+            } else {
+                this.JogadorAtual = Cor.White;
+            }
         }
 
         private void colocarPecas()
         {
-            this.Tab.ColocarPeca(new Rei(Cor.White, this.Tab), new PosicaoXadrez('c',1).ToPosicao());
+            this.Tab.ColocarPeca(new Rei(Cor.White, this.Tab), new PosicaoXadrez('c', 1).ToPosicao());
             this.Tab.ColocarPeca(new Torre(Cor.White, this.Tab), new PosicaoXadrez('c', 2).ToPosicao());
+            this.Tab.ColocarPeca(new Torre(Cor.White, this.Tab), new PosicaoXadrez('b', 1).ToPosicao());
+            this.Tab.ColocarPeca(new Torre(Cor.White, this.Tab), new PosicaoXadrez('b', 2).ToPosicao());
+            this.Tab.ColocarPeca(new Torre(Cor.White, this.Tab), new PosicaoXadrez('d', 1).ToPosicao());
+            this.Tab.ColocarPeca(new Torre(Cor.White, this.Tab), new PosicaoXadrez('d', 2).ToPosicao());
 
             this.Tab.ColocarPeca(new Rei(Cor.Black, this.Tab), new PosicaoXadrez('c', 8).ToPosicao());
         }
